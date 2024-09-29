e8cbadf1c15db54113ca7368149c7f6e2ec46f4e
Enrique Prada <rprada_lima@sunarp.gob.pe>
2024-09-29 01:50:58 -0500

    Actualizacion backend deploy

--- src/main/java/pe/edu/usmp/bot/app/MsEpicsBotApplication.java
+++ src/main/java/pe/edu/usmp/bot/app/MsEpicsBotApplication.java
@@ -2,19 +2,10 @@ package pe.edu.usmp.bot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MsEpicsBotApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MsEpicsBotApplication.class);
-	}
-	
+public class MsEpicsBotApplication  {
 	public static void main(String[] args) {
 		SpringApplication.run(MsEpicsBotApplication.class, args);
 	}
-	
 }
